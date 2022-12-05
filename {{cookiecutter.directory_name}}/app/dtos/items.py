acfc73ae794b7715b2acf2e9232071b505830883
shivanshkaushikk <98679261+shivanshkaushikk@users.noreply.github.com>
2022-12-05 21:05:53 +0530

    Delete {{cookiecutter.directory_name}}/app directory

--- {{cookiecutter.directory_name}}/app/dtos/items.py
+++
@@ -1,9 +0,0 @@
-from typing import Union
-from pydantic import BaseModel
-
-
-class Item(BaseModel):
-    name: str
-    description: Union[str, None] = None
-    price: float
-    tax: Union[float, None] = None
